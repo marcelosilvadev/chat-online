b20504d3047e526c173f5854edeec09b081dda96
Marcelo Silva <marcelo.silva@luizalabs.com>
2019-02-15 17:22:22 -0200

    First Commit Project

---
+++ lib/main.dart
@@ -0,0 +1,271 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+void main() async {
+  //Firestore.instance.collection("mensagens").document().setData({"from":"Marcelo", "texto": "Eae cachorro loko "}); //Para inserir uma nova informação  no banco Firebase
+
+  //DocumentSnapshot snapshot = await  Firestore.instance.collection("mensagens").document("Marcelo").get(); //Busca  uma informação informando seu nome de Document
+  //print(snapshot.data);
+
+  //QuerySnapshot snapshot = await Firestore.isntance.collection("usuarios").getdocuments(); //Leitura de todos os documents da colection
+  //for(DocumentSnapshot doc in snapshot.documents){
+  //  print(doc.data);
+  //}
+
+  //Firestore.instance.collection("mensagens").snapshots().listen((snapshot){ //Atualiza a lista sempre  que há alterações no banco.
+  //print(snapshot.documents);
+  //for(DocumentSnapshot doc in snapshot.documents){
+  //print(doc.data);
+  //}
+  //});
+
+  runApp(MyApp());
+}
+
+//Declarando Temas  para  android/IOS
+final ThemeData kIOSTheme = ThemeData(
+  primaryColor: Colors.grey[100],
+  primarySwatch: Colors.orange,
+  primaryColorBrightness: Brightness.light,
+);
+
+final ThemeData kDefaultTheme = ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
+
+final googleSignIn = GoogleSignIn();
+final auth = FirebaseAuth.instance;
+
+_ensureLoggedIn() async {
+  GoogleSignInAccount user = googleSignIn.currentUser;
+  if (user == null) {
+    user = await googleSignIn.signInSilently();
+  }
+
+  if (user == null) {
+    user = await googleSignIn.signIn();
+  }
+
+  if (await auth.currentUser() == null) {
+    GoogleSignInAuthentication credentials =
+        await googleSignIn.currentUser.authentication;
+    await auth.signInWithGoogle(
+        idToken: credentials.idToken, accessToken: credentials.accessToken);
+  }
+}
+
+_handleSubmitted(String text) async{
+  await  _ensureLoggedIn();
+  _sendMessage(text: text);
+}
+void _sendMessage({String text, String imgUrl}){
+  Firestore.instance.collection("messages").add(
+    {
+      "text": text,
+      "imgUrl": imgUrl,
+      "senderName": googleSignIn.currentUser.displayName,
+      "senderPhotoUrl": googleSignIn.currentUser.photoUrl
+    }
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Chat App", //Insere nome do App na aba
+      debugShowCheckedModeBanner: false, //Retira o banner DEBUG do  app
+      theme: Theme.of(context).platform == TargetPlatform.iOS
+          ? kIOSTheme
+          : kDefaultTheme,
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: false,
+      top: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Chat App"),
+          centerTitle: true,
+          elevation:
+              Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: StreamBuilder( //Refaz os widgets sem que há uma alteração
+                  stream: Firestore.instance.collection("messages").snapshots(),
+                  builder: (context, snapshot){
+                    switch(snapshot.connectionState){
+                      case ConnectionState.none:
+                      case ConnectionState.waiting:
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      default:
+                        return ListView.builder(
+                          reverse: true,
+                          itemCount: snapshot.data.documents.length,
+                          itemBuilder: (context, index){
+                            List r = snapshot.data.documents.reversed.toList();
+                            return ChatMessage(r[index].data);
+                          },
+                        );
+                    }
+                  }
+              )
+            ),
+            Divider(
+              height: 1.0,
+            ),
+            Container(
+              decoration: BoxDecoration(color: Theme.of(context).cardColor),
+              child: TextComposer(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TextComposer extends StatefulWidget {
+  @override
+  _TextComposerState createState() => _TextComposerState();
+}
+
+class _TextComposerState extends State<TextComposer> {
+  final  _textController = TextEditingController();
+
+  bool _isComposing = false;
+
+  void _reset(){
+    _textController.clear();
+    setState(() {
+      _isComposing = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: Theme.of(context).platform == TargetPlatform.iOS
+            ? BoxDecoration(
+                border: Border(top: BorderSide(color: Colors.grey[200])))
+            : null,
+        child: Row(
+          children: <Widget>[
+            Container(
+              child:
+                  IconButton(icon: Icon(Icons.photo_camera), onPressed: () async{
+                    await  _ensureLoggedIn();
+                    File imgFile = await ImagePicker.pickImage(source: ImageSource.camera);
+                    if(imgFile == null) return;
+                    StorageUploadTask task = FirebaseStorage.instance.ref().
+                    child(googleSignIn.currentUser.id.toString() +
+                        DateTime.now().millisecondsSinceEpoch.toString()).putFile(imgFile);
+                    StorageTaskSnapshot taskSnapshot = await task.onComplete;
+                    String url = await taskSnapshot.ref.getDownloadURL();
+                    _sendMessage(imgUrl: url);
+                  }),
+            ),
+            Expanded(
+              child: TextField(
+                controller: _textController,
+                decoration:
+                    InputDecoration.collapsed(hintText: "Enviar uma Mensagem"),
+                onChanged: (text) {
+                  setState(() {
+                    _isComposing = text.length > 0;
+                  });
+                },
+                onSubmitted: (text){
+                  _handleSubmitted(text);
+                  _reset();
+                },
+              ),
+            ),
+            Container(
+                margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                child: Theme.of(context).platform == TargetPlatform.iOS
+                    ? CupertinoButton(
+                        child: Text("Enviar"),
+                        onPressed: _isComposing ? () {
+                          _handleSubmitted(_textController.text);
+                          _reset();
+                        } : null,
+                      )
+                    : IconButton(
+                        icon: Icon(Icons.send),
+                        onPressed: _isComposing ? () {
+                          _handleSubmitted(_textController.text);
+                          _reset();
+                        } : null,
+                      ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+
+  final Map<String, dynamic> data;
+
+  ChatMessage(this.data);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+      child: Row(
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  data["senderPhotoUrl"]),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  data["senderName"],
+                  style: Theme.of(context).textTheme.subhead,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  child: data["imgUrl"] !=  null ? Image.network(data["imgUrl"], width: 250.0,): Text(data["text"]),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
